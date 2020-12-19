a898b19a7aabcb57cc4327c0b50a64aa98f7252b
pepeworm <impyworm@gmail.com>
2020-12-18 20:15:25 -0700

    Complete login route

--- app.js
+++ app.js
@@ -5,9 +5,6 @@
 require("dotenv").config();
 const express = require("express");
 const _ = require("lodash");
-// const session = require("express-session");
-// const passport = require("passport");
-// const passportLocalMongoose = require("passport-local-mongoose");
 const dateTime = require("./dateTime");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
@@ -21,21 +18,12 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//     session({
-//         secret: "This is a secret",
-//         resave: false,
-//         saveUninitialized: false,
-//     })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 // * Mongoose
 
 mongoose.connect(
     "mongodb://localhost:27017/homeworkAgenda",
-    // "mongodb+srv://admin-pepeworm:pepeworm@cluster0.v8jsq.mongodb.net/homeworkAgenda",
+    // process.env.MONGO,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -54,14 +42,7 @@ const accountSchema = new mongoose.Schema({
     },
 });
 
-// accountSchema.plugin(passportLocalMongoose);
-
 const Account = new mongoose.model("Account", accountSchema);
-
-// passport.use(Account.createStrategy());
-
-// passport.serializeUser(Account.serializeUser());
-// passport.deserializeUser(Account.deserializeUser());
 
 // * MongoDB (Subject Page)
 
@@ -106,23 +87,42 @@ app.route("/register")
                 if (foundEmail) {
                     res.render("register", { err: "emailErr" });
                 } else {
-                    bcrypt.hash(password, saltRounds, (err, hash) => {
+                    bcrypt.genSalt(saltRounds, (err, salt) => {
                         if (err) {
                             console.log(err);
                         } else {
-                        const newAccount = new Account({
-                            email: email,
-                            password: hash,
-                        });
+                            bcrypt.hash(password, salt, (err, hash) => {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    const newAccount = new Account({
+                                        email: email,
+                                        password: hash,
+                                    });
 
-                        newAccount.save((err) => {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                res.redirect("/home");
-                            }
-                        });
-                    }
+                                    newAccount.save((err) => {
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            Subject.find(
+                                                {},
+                                                (err, foundSubjects) => {
+                                                    if (err) {
+                                                        console.log(err);
+                                                    } else {
+                                                        res.render("home", {
+                                                            currentDate: dateTime.currentDate(),
+                                                            weekday: dateTime.weekday(),
+                                                            newSubjectItems: foundSubjects,
+                                                        });
+                                                    }
+                                                }
+                                            );
+                                        }
+                                    });
+                                }
+                            });
+                        }
                     });
                 }
             }
@@ -131,42 +131,68 @@ app.route("/register")
 
 // * Login Route
 
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-// * Home Route
-
-app.route("/home")
+app.route("/login")
     .get((req, res) => {
-        Subject.find({}, (err, foundSubjects) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("home", {
-                    currentDate: dateTime.currentDate(),
-                    weekday: dateTime.weekday(),
-                    newSubjectItems: foundSubjects,
-                });
-            }
-        });
+        res.render("login", { err: "" });
     })
-
     .post((req, res) => {
-        const subject = req.body.newSubject;
+        const email = req.body.email;
+        const password = req.body.password;
 
-        const subjectItem = new Subject({
-            subjectNames: subject,
-        });
-
-        subjectItem.save((err) => {
+        Account.findOne({ email: email }, (err, foundEmail) => {
             if (err) {
                 console.log(err);
             } else {
-                res.redirect("/home");
+                if (!foundEmail) {
+                    res.render("login", { err: "emailErr" });
+                } else {
+                    bcrypt.compare(
+                        password,
+                        foundEmail.password,
+                        (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                if (result === true) {
+                                    Subject.find({}, (err, foundSubjects) => {
+                                        if (err) {
+                                            console.log(err);
+                                        } else {
+                                            res.render("home", {
+                                                currentDate: dateTime.currentDate(),
+                                                weekday: dateTime.weekday(),
+                                                newSubjectItems: foundSubjects,
+                                            });
+                                        }
+                                    });
+                                } else {
+                                    res.render("login", { err: "passwordErr" });
+                                }
+                            }
+                        }
+                    );
+                }
             }
         });
     });
+    
+// * Home Route
+
+app.route("/home").post((req, res) => {
+    const subject = req.body.newSubject;
+
+    const subjectItem = new Subject({
+        subjectNames: subject,
+    });
+
+    subjectItem.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/home");
+        }
+    });
+});
 
 // * /deleteSubject Route
 
